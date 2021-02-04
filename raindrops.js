a58909fe1e5646492ca6dc2e636c84019679034b
Jadon-dsouza <74820224+Jadon-dsouza@users.noreply.github.com>
2021-02-04 15:13:08 +0000

    Add files via upload

---
+++ raindrops.js
@@ -0,0 +1,15 @@
+constructor(x,y,width,height) 
+    var options = {
+        isStatic: true
+    }
+    this.body = Bodies.elipse (x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  
+  display()
+    var pos =this.body.position;
+    elipseMode(CENTER);
+    fill("blue");
+    elipse(pos.x, pos.y, this.width, this.height);
+  
